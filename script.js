bf32b28c2ce402fab516d64b0fcf657415c662fd
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-26 13:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+// const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+// function hasKey(key) {
+//   //   write your code here
+	const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  return key in sampleObject;
 }
+
+// console.log(hasKey("red"));    // true
+// console.log(hasKey("blue"));   // false
+// console.log(hasKey("white"));  // true
+
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
